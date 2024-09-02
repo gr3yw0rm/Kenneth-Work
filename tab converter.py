4fec5ddf39432e91ea48b63976d4aabeb13ba4f8
Calvin <calvinmdelrosario@gmail.com>
2024-09-02 21:01:16 +0800

    Changed delimiter to vertical bar

--- tab converter.py
+++ tab converter.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-filename = 'D500_IT0008vtest.txt'
+filename = 'D500_PA0008_Septv2.txt'
 
 # reads as tab delimited file
-data = pd.read_csv(filename, skiprows=0, sep='\\t', index_col=False, engine='python')
+data = pd.read_csv(filename, skiprows=0, sep='|', index_col=False, engine='python')
 
 # removes quotes from data
 data = data.replace('"', '', regex=True).replace('    "', '', regex=True)
